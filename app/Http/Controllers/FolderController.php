de406727f2a58be5f5b5621a3efec3f1a2abebdd
whitia <whitia7@gmail.com>
2020-02-26 08:22:35 +0900

    Added creating folder

---
+++ app/Http/Controllers/FolderController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Folder;
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateFolder;
+
+class FolderController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }
+
+    public function create(CreateFolder $request)
+    {
+        $folder = new Folder();
+        $folder->title = $request->title;
+        $folder->save();
+
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', function () {
 });
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
