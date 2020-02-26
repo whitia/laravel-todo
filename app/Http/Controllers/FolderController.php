f9c1f4d34a00da4ffeabb4d9ac7fe7b7586f6da8
whitia <whitia7@gmail.com>
2020-02-26 20:37:46 +0900

    Modified auth

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateFolder;
+use Illuminate\Support\Facades\Auth;
 
 class FolderController extends Controller
 {
@@ -17,7 +18,7 @@ class FolderController extends Controller
     {
         $folder = new Folder();
         $folder->title = $request->title;
-        $folder->save();
+        Auth::user()->folders()->save($folder);
 
         return redirect()->route('tasks.index', [
             'id' => $folder->id,
